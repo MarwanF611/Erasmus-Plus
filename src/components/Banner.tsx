1f6f569af6f1c3e6e5f64e0abc0b584709ec1e50
Marwan Fikri <marwan.fikri200@gmail.com>
2023-06-04 01:59:29 +0200

    fixing minor bugs

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,16 @@
+import background from "../assets/circle-scatter-haikei.svg";
+
 export default function Banner() {
   return (
-    <section className="bg-gray-900 text-white">
+    <section
+      className="bg-gray-900 text-white"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
         <div className="mx-auto max-w-3xl text-center">
           <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
