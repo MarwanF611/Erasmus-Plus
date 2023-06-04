ab7933ba9b264535ee4d147f85dee47d681c9f96
Marwan Fikri <marwan.fikri200@gmail.com>
2023-06-04 23:54:43 +0200

    Change Navbar and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -54,7 +54,7 @@ export default function () {
           </div>
 
           <p className="mt-4 text-center text-sm text-gray-500 dark:text-gray-400 lg:mt-0 lg:text-right">
-            Copyright &copy; 2022. All rights reserved.
+            Marwan Fikri &copy; 2023. All rights reserved.
           </p>
         </div>
       </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -23,59 +23,8 @@ export default function NavBar() {
           <div className="md:flex md:items-center md:gap-12">
             <nav aria-label="Site Nav" className="hidden md:block">
               <ul className="flex items-center gap-6 text-sm">
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="/"
-                  >
-                    About
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="/"
-                  >
-                    Careers
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="/"
-                  >
-                    History
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="/"
-                  >
-                    Services
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="/"
-                  >
-                    Projects
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                    href="/"
-                  >
-                    Blog
-                  </a>
-                </li>
+                
+                
               </ul>
             </nav>
 
@@ -83,17 +32,19 @@ export default function NavBar() {
               <div className="sm:flex sm:gap-4">
                 <a
                   className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow dark:hover:bg-teal-500"
-                  href="/"
+                  href="https://github.com/MarwanF611"
+                  target="_Blank"
                 >
-                  Login
+                  GitHub
                 </a>
 
                 <div className="hidden sm:flex">
                   <a
                     className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 dark:bg-gray-800 dark:text-white dark:hover:text-white/75"
-                    href="/"
+                    href="https://www.instagram.com/f.mar06/"
+                    target="_Blank"
                   >
-                    Register
+                    Instagram
                   </a>
                 </div>
               </div>
